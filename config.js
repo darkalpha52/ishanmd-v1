961273b18ed224815032ee5580c8a062416f1675
darkalpha52 <ishanrandeepa52@gmail.com>
2025-02-15 00:28:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "v7QGXTrT#ehli8n-tnGWf4PYuPhcvCmPQaxBR5aheweXqhDMZ89M",
+  SESSION_ID: process.env.SESSION_ID || "XjAQ2LSC#arMylQrImW4vngYNnmCccxBFSVCZ_Hr-FX8GQcSkhEs",
   MONGODB: process.env.MONGODB || "mongodb://mongo:XDcKILNbQFpGqKdEXlcRYelMufekBiJD@junction.proxy.rlwy.net:17040",
   OWNER_NUM: process.env.OWNER_NUM || "94761279619",
 };
