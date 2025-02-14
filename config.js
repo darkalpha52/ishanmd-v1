6fc04adf24f97798db5163219a571ffd6a8b3cb5
darkalpha52 <ishanrandeepa52@gmail.com>
2025-02-14 23:51:14 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XjAQ2LSC#arMylQrImW4vngYNnmCccxBFSVCZ_Hr-FX8GQcSkhEs",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
