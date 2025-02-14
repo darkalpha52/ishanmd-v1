d607838052bff117c152827d8a0a3e6c2a6346ad
darkalpha52 <ishanrandeepa52@gmail.com>
2025-02-14 23:54:50 +0530

    Add files via upload

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,46 +1,20 @@
-const { cmd, commands } = require('../command');
-const os = require("os");
-const { runtime } = require('../lib/functions');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
-    alias: ["status", "runtime", "uptime"],
-    desc: "Check uptime and system status",
+    desc: "Check bot online or no.",
     category: "main",
-    react: "📟",
     filename: __filename
 },
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        // Generate system status message
-        const status = `*《☫ 𝐈 𝐒 𝐇 𝐀 𝐍  𝐌 𝐃 ☫》* 
+async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const config = await readEnv();
+return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
 
-▫️️*⏳Uptime*:  ${runtime(process.uptime())} 
-▫️️*📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-▫️️*⚙️ HostName*: ${os.hostname()}
-▫️️*👨‍💻 Owner*: 𝙸𝚂𝙷𝙰𝙽 𝙼𝙳
-▫️️*🧬 Version*: 1.0.0 BETA
-
-> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɪsʜᴀɴ-ᴍᴅ`;
-
-        // Send the status message with an image
-        await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/svN0r7rn/3841.jpg` },  // Image URL
-            caption: status,
-            contextInfo: {
-                mentionedJid: [m.sender],
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363391262285507@newsletter',
-                    newsletterName: '𝙸𝚂𝙷𝙰𝙽 𝙼𝙳',
-                    serverMessageId: 143
-                }
-            }
-        }, { quoted: mek });
-
-    } catch (e) {
-        console.error("Error in alive command:", e);
-        reply(`An error occurred: ${e.message}`);
-    }
-});
