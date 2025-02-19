e6da0163bfb4757b89e1a4aac0fc631d893af3d1
darkalpha52 <ishanrandeepa52@gmail.com>
2025-02-19 21:25:51 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({
-    path: './config.env'
-});
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
-
 module.exports = {
-   SESSION_ID: process.env.SESSION_ID === undefined ? 'nIJ1wRAS#j_7030J-3sOqoW126Q-FHXtVesfjrV5rIqpYV3ye1n4' : process.env.SESSION_ID, 
-   
+ SESSION_ID: process.env.SESSION_ID ||"id",
+ OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",   
+    
 };
