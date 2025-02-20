54228f7c1c792c1eb61b14a83385b51001b968db
darkalpha52 <ishanrandeepa52@gmail.com>
2025-02-20 19:26:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
  SESSION_ID: process.env.SESSION_ID ||"𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=a7hEgaaT#sR0O1aFk7_4tV37omgBAB_lfNU5wFFWveZB0D-T9HPQ",
- OWNER_NUM: process.env.OWNER_NUM || "94761279619",   
+    
     
 };
